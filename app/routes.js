ed504630892a531939ef32cb697e673e0bac6013
desshub <desshub95@gmail.com>
2018-03-26 11:14:04 +0300

    added facebook login

--- app/routes.js
+++ app/routes.js
@@ -44,6 +44,22 @@ module.exports = function(app, passport) {
     }));
 
     // =====================================
+    // FACEBOOK ROUTES =====================
+    // =====================================
+    // route for facebook authentication and login
+    app.get('/auth/facebook', passport.authenticate('facebook', { 
+      scope : ['public_profile', 'email']
+    }));
+
+    // handle the callback after facebook has authenticated the user
+    app.get('/auth/facebook/callback',
+        passport.authenticate('facebook', {
+            successRedirect : '/profile',
+            failureRedirect : '/'
+        }));
+
+
+    // =====================================
     // PROFILE SECTION =====================
     // =====================================
     // we will want this protected so you have to be logged in to visit
